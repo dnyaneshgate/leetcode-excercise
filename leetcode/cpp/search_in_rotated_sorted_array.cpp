1604e4778b22a2a356d8fb0e0221e192e7e2285e
Dnyanesh Gate <dnyanesh.gate@druva.com>
2020-04-26 22:23:23 +0530

    covid-19 leetcode 30 day challenge

--- leetcode/cpp/search_in_rotated_sorted_array.cpp
+++ leetcode/cpp/search_in_rotated_sorted_array.cpp
@@ -32,14 +32,113 @@ auto speedup = []() {
     return nullptr;
 }();
 
-int search(vector<int> &nums, int target, int start, int end) {
+int search_rec(vector<int> &nums, int target, int start, int end) {
     if (start > end) {
         return -1;
     }
-    return 0;
+
+    if (start == end && nums[start] != target) {
+        return -1;
+    }
+
+    if (start == end && nums[start] == target) {
+        return start;
+    }
+
+    int mid = (start + end) / 2;
+
+    int found = search_rec(nums, target, start, mid);
+    if (found != -1) {
+        return found;
+    }
+
+    found = search_rec(nums, target, mid + 1, end);
+    if (found != -1) {
+        return found;
+    }
+
+    return -1;
+}
+
+int search1(vector<int> &nums, int target) {
+    return search_rec(nums, target, 0, nums.size() - 1);
+}
+
+int search2(vector<int> &nums, int target) {
+    for (int i = 0; i < nums.size(); i += 2) {
+        if (nums[i] == target) {
+            return i;
+        }
+        if (i < nums.size() - 1 && nums[i + 1] == target) {
+            return i + 1;
+        }
+    }
+    return -1;
+}
+
+int findPivot(vector<int> &nums, int low, int high) {
+    if (low > high) {
+        return -1;
+    }
+
+    if (low == high) {
+        return -1;
+    }
+
+    int mid = (low + high) / 2;
+
+    if (mid < high && nums[mid] > nums[mid + 1]) {
+        return mid + 1;
+    }
+
+    if (mid > low && nums[mid - 1] > nums[mid]) {
+        return mid;
+    }
+
+    if (nums[mid] > nums[high]) {
+        return findPivot(nums, mid + 1, high);
+    }
+
+    return findPivot(nums, low, mid);
+}
+
+int bisearch(vector<int> &nums, int target, int low, int high) {
+    if (low > high) {
+        return -1;
+    }
+
+    if (low == high && nums[low] == target) {
+        return low;
+    }
+
+    int mid = (low + high) / 2;
+
+    if (nums[mid] == target) {
+        return mid;
+    }
+
+    if (target < nums[mid]) {
+        return bisearch(nums, target, low, mid - 1);
+    }
+
+    return bisearch(nums, target, mid + 1, high);
 }
 
 int search(vector<int> &nums, int target) {
+    int p = findPivot(nums, 0, nums.size() - 1);
+    if (p == -1) {
+        return bisearch(nums, target, 0, nums.size() - 1);
+    }
+
+    if (nums[p] == target) {
+        return p;
+    }
+
+    if (nums[0] <= target) {
+        return bisearch(nums, target, 0, p - 1);
+    }
+
+    return bisearch(nums, target, p + 1, nums.size() - 1);
 }
 
 int main() {
@@ -50,12 +149,14 @@ int main() {
     };
 
     vector<test_t> tests = {
-        {{4, 5, 6, 7, 0, 1, 2}, 4, 0},
+        {{4, 5, 6, 7, 8, 0, 1, 2}, 4, 0},
         {{4, 5, 6, 7, 0, 1, 2}, 3, -1},
+        {{8, 1, 2, 3, 4, 5, 6}, 0, -1},
     };
 
     for (auto &t : tests) {
-        assert(search(t.nums, t.target) == t.expected);
+        cout << search(t.nums, t.target) << endl;
+        // assert(search(t.nums, t.target) == t.expected);
     }
 
     return 0;
